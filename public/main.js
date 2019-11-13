1df575b041175ff79d8f5fa24d20d3e2ee45ccb0
Georg Oehl <oehl@pobox.com>
2019-11-13 17:11:37 -0500

    Framework mostly done except mission content

--- public/main.js
+++ public/main.js
@@ -1,7 +1,55 @@
+const apodApiURL = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+const upcomingApiURL = 'https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming'
+let upcomingJSON
+let upcomingIndexPointer
+
+const qS = e => document.querySelector(e)
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  fetchApod()
+  fetchUpcoming()
+}
+
+const fetchApod = async () => {
+  const resp = await fetch(apodApiURL)
+  if (resp.status != 200) {
+    return
   }
+  const apod = await resp.json()
+  // qS('.aotdContainer').style.backgroundImage = 'url("' + apod.url + '")'
+  qS('.aotd').src = apod.url
+  qS('.copyright').textContent = 'copyright: ' + apod.copyright + ' | title: ' + apod.title
+}
+
+const fetchUpcoming = async () => {
+  const resp = await fetch(upcomingApiURL)
+  if (resp.status != 200) {
+    return
+  }
+  upcomingJSON = await resp.json()
+  upcomingIndexPointer = 0
+  populateMission()
+}
+
+const populateMission = () => {
+  if (typeof upcomingIndexPointer === 'undefined') {
+    return
+  }
+
+  const missionName = upcomingJSON[upcomingIndexPointer].mission_name
+  const missionDesc = upcomingJSON[upcomingIndexPointer].details
+  // qS('.missionCountdown').textContent = upcomingJSON[upcomingIndexPointer].details
+  const missionLaunch = (qS('.missionLaunchsite').textContent =
+    upcomingJSON[upcomingIndexPointer].launch_site.site_name_long)
+
+  qS('.missionName').textContent = missionName(null ? 'No mission name available yet' : missionName)
+  qS('.missionDescription').textContent = missionDesc(
+    null ? 'No description available yet' : missionDesc
+  )
+  // qS('.missionCountdown').textContent = missionName (null ? 'No mission name yet' : missionName)
+  qS('.missionLaunchsite').textContent = missionLaunch(
+    null ? 'No launch site available yet' : missionLaunch
+  )
 }
 
 document.addEventListener('DOMContentLoaded', main)
